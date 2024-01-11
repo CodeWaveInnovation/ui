06ea7d08089d368f7ea0ee88b10369418bae2953
Ignacio Avilés Cardenasso <nacho72001@gmail.com>
2024-01-11 13:14:57 -0300

    fix name and format

--- plopfile.mjs
+++ plopfile.mjs
@@ -1,30 +1,30 @@
 export default function (plop) {
+  plop.setHelper("FirstLetterCapital", (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  });
 
-    plop.setHelper('FirstLetterCapital', (str) => { 
-        return str.charAt(0).toUpperCase() + str.slice(1);
-    })
-
-    plop.setGenerator('Create Component', {
-        description: 'Create a new component',
-        prompts: [
-            {
-                type: 'input',
-                name: 'name',
-                message: 'What is the name of your component?: ',
-                validate: (value)=> ((/[a-z]/gi).test(value)) ? true : 'name is required'
-            }
-        ],
-        actions: [
-            {
-                type: 'add',
-                path: 'src/components/{{name}}/{{name}}.tsx',
-                templateFile: 'plop-templates/component.tsx.hbs'
-            },
-            {
-                type: 'add',
-                path: 'src/components/{{name}}/{{name}}.styles.css',
-                templateFile: 'plop-templates/styles.css.hbs'
-            },
-        ],
-    })
+  plop.setGenerator("Create Component", {
+    description: "Create a new component",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "What is the name of your component?: ",
+        validate: (value) =>
+          /[a-z]/gi.test(value) ? true : "name is required",
+      },
+    ],
+    actions: [
+      {
+        type: "add",
+        path: "src/components/{{name}}/{{name}}.tsx",
+        templateFile: "plop-templates/component.tsx.hbs",
+      },
+      {
+        type: "add",
+        path: "src/components/{{name}}/{{name}}.module.css",
+        templateFile: "plop-templates/styles.css.hbs",
+      },
+    ],
+  });
 }
